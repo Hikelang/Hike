a7c2a482efd493e5a7a3004193b3f2d3da1ab68f
adisalimgereev <adi.salimgereev25@fizmat.kz>
2021-12-13 13:16:42 +0600

    Well, add build.py file bindings for OS

--- gear/build/build.py
+++ gear/build/build.py
@@ -4,23 +4,45 @@ import os
 package_prefix = "gear/cmd"
 packages_to_test = ["gear/pkg/parser", "gear/pkg/lexer", "gear/pkg/docparser"]
 
+platforms = [
+    ('linux', '386'),
+    ('linux', 'amd64'),
+    ('linux', 'arm'),
+    ('linux', 'arm64'),
+    ('windows', '386'),
+    ('windows', 'amd64')
+]
+
 print("⚙️  hike build started")
 
 threads = []
 
 def test(package):
     os.system(f"go test {package}")
+    print(f"😃 finished testing thread [package={package}]")
 
-def compile(package):
-    os.system(f"go build {package}")
+def compile(package, platform):
+    os.system(f"env GOOS={platform[0]} GOARCH={platform[1]} go build {package}")
 
 for package in packages_to_test:
     Thread(target=test, args=(package, )).start()
-    print(f"⚙️  >>> testing thread started [package={package}]")
+    print(f"test {package}")
+
+try:
+    os.mkdir(".build")
+except:
+    pass
 
 for directory in os.listdir("cmd"):
     package = f"{package_prefix}/{directory}"
-    print(f"⚙️  >>> found {package}")
-    thread = Thread(target = compile, args=(package,))
-    thread.start()
-    print(f"⚙️  >>> compilation thread started [package={package}]")
+    for platform in platforms:
+        try:
+            os.mkdir(f".build/{platform[0]}.{platform[1]}")
+        except:
+            pass
+        thread = Thread(target = compile, args=(package, platform))
+        thread.start()
+        print(f"compile {directory}, os: {platform[0]}, arch: {platform[1]}")
+        for file in os.listdir("."):
+            if file.split(".")[-1] == directory:
+                os.system(f"mv {file} .build/{platform[0]}.{platform[1]}/{file}")
